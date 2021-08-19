8dae4b41e98c181a4d9080a512ecd493ffc24a1a
stefysoet <stefysoet@hotmail.com>
2021-08-19 09:20:46 -0500

    Post

--- src/pages/news.js
+++ src/pages/news.js
@@ -70,4 +70,108 @@ const newsPage = `
     </div>
   </section>
 `;
+const db = firebase.firestore();
+
+const taskForm = document.getElementById("task-form");
+const tasksContainer = document.getElementById("tasks-container");
+
+let editStatus = false;
+let id = '';
+
+/**
+ * Save a New Task in Firestore
+ * @param {string} title the title of the Task
+ * @param {string} description the description of the Task
+ */
+const saveTask = (description) =>
+  db.collection("tasks").doc().set({
+    description,
+  });
+
+const getTasks = () => db.collection("tasks").get();
+
+const onGetTasks = (callback) => db.collection("tasks").onSnapshot(callback);
+
+const deleteTask = (id) => db.collection("tasks").doc(id).delete();
+
+const getTask = (id) => db.collection("tasks").doc(id).get();
+
+const updateTask = (id, updatedTask) => db.collection('tasks').doc(id).update(updatedTask);
+
+window.addEventListener("DOMContentLoaded", async (e) => {
+  onGetTasks((querySnapshot) => {
+    tasksContainer.innerHTML = "";
+
+    querySnapshot.forEach((doc) => {
+      const task = doc.data();
+
+      tasksContainer.innerHTML += `<div class="card card-body mt-2 border-primary">
+    <p>${task.description}</p>
+    <div>
+      <button class="btn btn-primary btn-delete" data-id="${doc.id}">
+        🗑 Delete
+      </button>
+      <button class="btn btn-secondary btn-edit" data-id="${doc.id}">
+        🖉 Edit
+      </button>
+    </div>
+  </div>`;
+    });
+
+    const btnsDelete = tasksContainer.querySelectorAll(".btn-delete");
+    btnsDelete.forEach((btn) =>
+      btn.addEventListener("click", async (e) => {
+        console.log(e.target.dataset.id);
+        try {
+          await deleteTask(e.target.dataset.id);
+        } catch (error) {
+          console.log(error);
+        }
+      })
+    );
+
+    const btnsEdit = tasksContainer.querySelectorAll(".btn-edit");
+    btnsEdit.forEach((btn) => {
+      btn.addEventListener("click", async (e) => {
+        try {
+          const doc = await getTask(e.target.dataset.id);
+          const task = doc.data();
+          taskForm["task-description"].value = task.description;
+
+          editStatus = true;
+          id = doc.id;
+          taskForm["btn-task-form"].innerText = "Save";
+
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    });
+  });
+});
+
+taskForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const description = taskForm["task-description"];
+
+  try {
+    if (!editStatus) {
+      await saveTask(description.value);
+    } else {
+      await updateTask(id, {
+        description: description.value,
+      })
+
+      editStatus = false;
+      id = '';
+      taskForm['btn-task-form'].innerText = 'Post';
+    }
+
+    taskForm.reset();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 export default newsPage;
