dd5ac4229a90ea9465dca9842c830e50fc6bd4ae
alissonsegura <alissonicoleseguratarazona@gmail.com>
2021-09-26 18:02:02 -0500

    fix routes with subdir

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { routes } from './lib/router.js';
 
 // const root = document.querySelector('#root');
 const pathName = window.location.pathname;
-let pathNameNoSlash = pathName.replace('/', '') || 'login';
-pathNameNoSlash = pathNameNoSlash.replace('LIM015-social-network/src/', '');
+let pathNameNoSlash = pathName.replace('LIM015-social-network/src/', '');
+pathNameNoSlash = pathName.replace('/', '') || 'login';
 export const inicialize = () => {
   routes[`${pathNameNoSlash}`]();
 };
