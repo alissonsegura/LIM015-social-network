3717abaef4a814d42be5c1ac25f8e819fbd9e488
alissonsegura <alissonicoleseguratarazona@gmail.com>
2021-09-26 18:05:04 -0500

    fix: routes

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { routes } from './lib/router.js';
 // const root = document.querySelector('#root');
 const pathName = window.location.pathname;
 let pathNameNoSlash = pathName.replace('LIM015-social-network/src/', '');
-pathNameNoSlash = pathName.replace('/', '') || 'login';
+pathNameNoSlash = pathNameNoSlash.replace('/', '') || 'login';
 export const inicialize = () => {
   routes[`${pathNameNoSlash}`]();
 };
