e8538cf573391246787725abeab57e362b6844b9
alissonsegura <alissonicoleseguratarazona@gmail.com>
2021-09-26 17:56:58 -0500

    fix routes

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { routes } from './lib/router.js';
 // const root = document.querySelector('#root');
 const pathName = window.location.pathname;
 let pathNameNoSlash = pathName.replace('/', '') || 'login';
-pathNameNoSlash = pathNameNoSlash.replace('LIM015-social-network/', '');
+pathNameNoSlash = pathNameNoSlash.replace('LIM015-social-network/src/', '');
 export const inicialize = () => {
   routes[`${pathNameNoSlash}`]();
 };
